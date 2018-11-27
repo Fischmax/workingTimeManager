4dcc25304eca16224753221da518e13b98ebfde2
Maximilian Fischer <35427013+fischmax@users.noreply.github.com>
2018-11-27 10:46:31 +0100

    Added first path/view and database model for work_time

--- workingTimeManagerBase/workingTimeManager/models.py
+++ workingTimeManagerBase/workingTimeManager/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Worktime(models.Model):
+    # User-ID
+    time_from = models.TimeField(default=None)
+    time_to = models.TimeField(default=None)
+    minutes_total = models.IntegerField(default=0)
+    minutes_break = models.IntegerField(default=0)
+    balance = models.IntegerField(default=0)
+    date = models.DateField(default=None)
