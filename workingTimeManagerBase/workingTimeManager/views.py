6c6ef95407be4e5b62e60607d128129c2e3593cf
Maximilian Fischer <35427013+fischmax@users.noreply.github.com>
2019-01-04 14:20:24 +0100

    Implemented variable break_time(Issue #3) Fixed wrong argument in errorhandling

--- workingTimeManagerBase/workingTimeManager/views.py
+++ workingTimeManagerBase/workingTimeManager/views.py
@@ -19,12 +19,18 @@ def add_time(request):
     # print(time_to_add.time_from)
     time_to_add.time_to = request.POST['time_to']
     time_to = time.strptime(time_to_add.time_to, '%H:%M')
+    if 'minutes_break' in request.POST:
+        try:
+            time_to_add.minutes_break = int(request.POST['minutes_break'])
+        except ValueError:
+            return HttpResponse(status=400)
+    else:
+        time_to_add.minutes_break = 45
     # print(time_to_add.time_to)
     if time_to < time_from:
-        return HttpResponse(code=400)
+        return HttpResponse(status=400)
     else:
         time_to_add.balance = (time_to.tm_hour*60 - time_from.tm_hour*60) + (time_to.tm_min - time_from.tm_min)
-        time_to_add.minutes_break = 45
         time_to_add.minutes_total = time_to_add.balance + time_to_add.minutes_break
         time_to_add.balance = time_to_add.balance - (time_to_add.minutes_break + 8*60)
         # TODO: variable, automatic
